e22b8adf9a87881f830a7b82afabd73017c71437
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-13 19:08:39 -0300

    added comment model

---
+++ models/comment.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  creator: { type: Schema.Types.ObjectId, ref: "Users", require: true },
+  content: { text: { type: String, required: true } },
+  likes: [{ type: Schema.Types.ObjectId }],
+  timestamp: { type: Date, required: true, default: Date.now },
+});
+
+exports.module = mongoose.Schema("Comments", commentSchema);
