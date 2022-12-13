6384145a7d418580617b30d42fd6e4165679045f
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-13 19:06:30 -0300

    added user model and connected mongodb to the app

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+const mongoDB = process.env.MONGODB_URL;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error"));
 
 app.get("/", (req, res) => res.json({ response: "Hello World" }));
 
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true, minLength: 8 },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  creation_date: { type: Date, required: true, default: Date.now },
+  gender: { type: String, enum: ["male", "female", "other"] },
+  birthday: { type: Date, required: true },
+  refresh_token: { type: String, default: "" },
+  friend_requests: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  friend_list: [{ type: Schema.Types.ObjectId, ref: "Users" }],
+})
+
+userSchema.set("toJSON", {
+  transform: function (doc, ret, options) {
+    delete ret.refresh_token;
+    return ret;
+  },
+});
+exports.module = mongoose.model("Users", userSchema);
