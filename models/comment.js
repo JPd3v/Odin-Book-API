308a40f0b3efcf801742b85f135064180a4f89c1
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-15 07:37:12 -0300

    fixed typos and added edited field

--- models/comment.js
+++ models/comment.js
@@ -4,8 +4,9 @@ const Schema = mongoose.Schema;
 const commentSchema = new Schema({
   creator: { type: Schema.Types.ObjectId, ref: "Users", require: true },
   content: { text: { type: String, required: true } },
-  likes: [{ type: Schema.Types.ObjectId }],
+  edited: { type: Boolean, required: true, default: false },
+  likes: [{ type: Schema.Types.ObjectId, ref: "Users" }],
   timestamp: { type: Date, required: true, default: Date.now },
 });
 
-module.exports = mongoose.Schema("Comments", commentSchema);
+module.exports = mongoose.model("Comments", commentSchema);
--- models/post.js
+++ models/post.js
@@ -4,9 +4,10 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   creator: { type: Schema.Types.ObjectId, ref: "Users", required: true },
   content: { text: { type: String, required: true } },
-  likes: [{ type: Schema.Types.ObjectId }],
+  edited: { type: Boolean, required: true, default: false },
+  likes: [{ type: Schema.Types.ObjectId, ref: "Users" }],
   timestamp: { type: Date, required: true, default: Date.now },
   comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
 });
 
-module.exports("Posts", postSchema);
+module.exports = mongoose.model("Posts", postSchema);
