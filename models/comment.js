76ea4c493a767e44263712d038b84e711a7c262f
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-16 06:50:58 -0300

    added replies and post_id fields used for...
    allowing posting replies to a comment  and using the post_id
    for deleting comment reference from  a post in case of deleting a
    comment

--- models/comment.js
+++ models/comment.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+const Posts = require("./post");
 const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
   creator: { type: Schema.Types.ObjectId, ref: "Users", require: true },
+  post_id: { type: String, required: true },
   content: { text: { type: String, required: true } },
   edited: { type: Boolean, required: true, default: false },
   likes: [{ type: Schema.Types.ObjectId, ref: "Users" }],
+  replies: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
   timestamp: { type: Date, required: true, default: Date.now },
 });
 
