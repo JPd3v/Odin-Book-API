d62490688cb2ca731de54680767bedbc5e919289
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-17 15:16:43 -0300

    added replies model, and fixed some typos

--- models/replies.js
+++ models/replies.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new Schema({
   creator: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+  comment_id: { type: String, required: true },
   post_id: { type: String, required: true },
   content: { text: { type: String, required: true } },
   edited: { type: Boolean, required: true, default: false },
   likes: [{ type: Schema.Types.ObjectId, ref: "Users" }],
-  replies: [{ type: Schema.Types.ObjectId, ref: "Replies" }],
   timestamp: { type: Date, required: true, default: Date.now },
 });
 
-module.exports = mongoose.model("Comments", commentSchema);
+module.exports = mongoose.model("Replies", commentSchema);
