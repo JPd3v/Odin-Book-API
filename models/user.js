108b13e9d8ce356aa6fdb5ab6292e136fd7c4d66
JPd3v <103145393+JPd3v@users.noreply.github.com>
2022-12-24 06:03:31 -0300

    added profile image to user model

--- models/user.js
+++ models/user.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const DEFAULT_PROFILE_IMG =
+  "https://res.cloudinary.com/dvnawerv1/image/upload/v1671863396/obbtgos8csgnzrplinzs.jpg";
+
 const userSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true, minLength: 8 },
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
+  profile_image: {
+    public_id: { type: String },
+    img: { type: String, required: true, default: DEFAULT_PROFILE_IMG },
+  },
   creation_date: { type: Date, required: true, default: Date.now },
   gender: { type: String, enum: ["male", "female", "other"] },
   birthday: { type: String, required: true },
