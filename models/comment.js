69673fe60dde3033b958c8f5cf0abe29cc733cfc
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-02-13 07:50:43 -0300

    adds cascade delete of post, comments and replies
    using mongoose pre middleware

--- models/comment.js
+++ models/comment.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const Posts = require("./post");
 const Schema = mongoose.Schema;
+const Replies = require("./replies");
 
 const commentSchema = new Schema({
   creator: { type: Schema.Types.ObjectId, ref: "Users", required: true },
@@ -10,6 +10,24 @@ const commentSchema = new Schema({
   likes: [{ type: Schema.Types.ObjectId, ref: "Users" }],
   replies: [{ type: Schema.Types.ObjectId, ref: "Replies" }],
   timestamp: { type: Date, required: true, default: Date.now },
+});
+
+commentSchema.pre("deleteMany", async function () {
+  try {
+    const postId = this.getQuery().post_id;
+    await Replies.deleteMany({ post_id: postId });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+commentSchema.pre("findOneAndDelete", async function () {
+  try {
+    const commentId = this.getQuery()._id;
+    await Replies.deleteMany({ comment_id: commentId });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = mongoose.model("Comments", commentSchema);
--- models/post.js
+++ models/post.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Comments = require("./comment");
+const cloudinaryConfig = require("../config/cloudinaryconfig");
 
 const postSchema = new Schema({
   creator: { type: Schema.Types.ObjectId, ref: "Users", required: true },
@@ -11,6 +13,22 @@ const postSchema = new Schema({
   likes: [{ type: Schema.Types.ObjectId, ref: "Users" }],
   timestamp: { type: Date, required: true, default: Date.now },
   comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
+});
+
+postSchema.pre("findOneAndDelete", async function () {
+  try {
+    const post = await this.model.findById(this.getQuery()._id);
+
+    await Promise.all(
+      post.content.images.map((image) =>
+        cloudinaryConfig.uploader.destroy(image.public_id)
+      )
+    );
+
+    await Comments.deleteMany({ post_id: post._id });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = mongoose.model("Posts", postSchema);
