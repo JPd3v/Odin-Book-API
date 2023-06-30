769a2931064ced19b0d7b17e1d7dde9364e1a4dd
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-06-30 20:25:54 -0300

    changes post schema model
    -removes array of likes ids
    -add virtual properties for like count and comment count

--- models/post.js
+++ models/post.js
@@ -2,16 +2,33 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Comments = require("./comment");
 const cloudinaryConfig = require("../config/cloudinaryconfig");
+const PostLikes = require("./postLikes");
 
-const postSchema = new Schema({
-  creator: { type: Schema.Types.ObjectId, ref: "Users", required: true },
-  content: {
-    text: { type: String, required: true },
-    images: [{ public_id: { type: String }, img: { type: String } }],
+const postSchema = new Schema(
+  {
+    creator: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+    content: {
+      text: { type: String, required: true },
+      images: [{ public_id: { type: String }, img: { type: String } }],
+    },
+    edited: { type: Boolean, required: true, default: false },
+    timestamp: { type: Date, required: true, default: Date.now },
   },
-  edited: { type: Boolean, required: true, default: false },
-  likes: [{ type: Schema.Types.ObjectId, ref: "Users" }],
-  timestamp: { type: Date, required: true, default: Date.now },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+postSchema.virtual("likesCount", {
+  ref: "Post_likes",
+  localField: "_id",
+  foreignField: "post_id",
+  count: true,
+});
+
+postSchema.virtual("commentCount", {
+  ref: "Comments",
+  localField: "_id",
+  foreignField: "post_id",
+  count: true,
 });
 
 postSchema.pre("findOneAndDelete", async function () {
@@ -25,6 +42,7 @@ postSchema.pre("findOneAndDelete", async function () {
     );
 
     await Comments.deleteMany({ post_id: post._id });
+    await PostLikes.deleteMany({ post_id: post._id });
   } catch (error) {
     console.log(error);
   }
