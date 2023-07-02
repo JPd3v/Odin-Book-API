c879ffa9e02e0ff45d7fe50bc1290b2d4e207a3d
JPd3v <103145393+JPd3v@users.noreply.github.com>
2023-07-02 02:05:49 -0300

    changes replies model schema
    -removed array of id on likes field
    -fix post_id, comment_id types
    -added likescount virtual

--- models/replies.js
+++ models/replies.js
@@ -1,15 +1,36 @@
 const mongoose = require("mongoose");
-const Posts = require("./post");
+const repliesLikes = require("./repliesLikes");
 const Schema = mongoose.Schema;
 
-const commentSchema = new Schema({
-  creator: { type: Schema.Types.ObjectId, ref: "Users", required: true },
-  comment_id: { type: String, required: true },
-  post_id: { type: String, required: true },
-  content: { text: { type: String, required: true } },
-  edited: { type: Boolean, required: true, default: false },
-  likes: [{ type: Schema.Types.ObjectId, ref: "Users" }],
-  timestamp: { type: Date, required: true, default: Date.now },
+const replySchema = new Schema(
+  {
+    creator: { type: Schema.Types.ObjectId, ref: "Users", required: true },
+    comment_id: { type: Schema.Types.ObjectId, ref: "Comments", required: true },
+    post_id: { type: Schema.Types.ObjectId, ref: "Posts", required: true },
+    content: { text: { type: String, required: true } },
+    edited: { type: Boolean, required: true, default: false },
+    timestamp: { type: Date, required: true, default: Date.now },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+replySchema.virtual("likesCount", {
+  localField: "_id",
+  foreignField: "reply_id",
+  ref: "reply_likes",
+  count: true,
 });
 
-module.exports = mongoose.model("Replies", commentSchema);
+replySchema.pre("findOneAndDelete", async function () {
+  try {
+    const replyId = this.getQuery()._id;
+    await repliesLikes.deleteMany({ reply_id: replyId });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = mongoose.model("Replies", replySchema);
